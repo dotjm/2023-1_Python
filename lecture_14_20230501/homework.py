5820858844d59f8fb313eadb449f2b5e7f6ab42f
정지민(Jeong Jimin) <jm040629@gmail.com>
2023-05-08 01:17:04 +0900

    Fix: homework fix
    과제 오류 수정 및 개선

--- lecture_14_20230501/homework.py
+++ lecture_14_20230501/homework.py
@@ -5,14 +5,19 @@
 
 stack = []
 while True:
+    print()
     print("- 모드 - \n1. 입력 2. 출력 3. 종료")
     mode = int(input("모드 숫자를 입력하시오 : "))
     if (mode == 1):
-        inval = input("입력할 숫자를 뛰어쓰기로 구분하여 입력")
+        inval = input("입력할 숫자를 뛰어쓰기로 구분하여 입력 : ")
         inlist = inval.split()
         stack = stack + inlist
     elif (mode == 2):
-        print("출력된 값 : ", stack.pop())
+        if (len(stack) >= 1):
+            print("출력된 값 : ", stack.pop())
+        else:
+            print("빈 스택이므로 출력 불가")
+        
     elif (mode == -1 or mode == 3):
         print("종료합니다. ")
         break
